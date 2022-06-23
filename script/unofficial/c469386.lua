3504df820e5bb56c84a0dd0ccd8396e7d912134e
SoobaXtheZ <104928372+SoobaXtheZ@users.noreply.github.com>
2022-06-23 12:17:21 +0200

    Add files via upload

--- script/unofficial/c469386.lua
+++ script/unofficial/c469386.lua
@@ -1,4 +1,5 @@
 --I PISSED ON THE MOON
+--created by sooba
 local s,id=GetID()
 function s.initial_effect(c)
 	--Activate
@@ -6,9 +7,14 @@ function s.initial_effect(c)
 	e1:SetCategory(CATEGORY_DESTROY)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
+	  e1:SetCost(s.cost)
 	e1:SetTarget(s.target)
 	e1:SetOperation(s.activate)
 	c:RegisterEffect(e1)
+end
+function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsCanRemoveCounter(tp,1,0,0x1f,20,REASON_COST) end
+	Duel.RemoveCounter(tp,1,0,0x1f,20,REASON_COST)
 end
 function s.filter(c)
 	return c:IsType(TYPE_SPELL+TYPE_TRAP+TYPE_MONSTER)
