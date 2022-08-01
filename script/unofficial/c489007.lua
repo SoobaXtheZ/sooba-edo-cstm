d115ee57b1742b47face84924247ab1212a23e4c
SoobaXtheZ <104928372+SoobaXtheZ@users.noreply.github.com>
2022-08-01 10:40:59 +0200

    Update c489007.lua

--- script/unofficial/c489007.lua
+++ script/unofficial/c489007.lua
@@ -4,13 +4,14 @@ local s,id=GetID()
 function s.initial_effect(c)
 	--Activate
 	local e1=Effect.CreateEffect(c)
-	e1:SetCategory(CATEGORY_DESTROY+CATEGORY_DRAW)
+	e1:SetCategory(CATEGORY_DESTROY)
 	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
-	e1:SetCountLimit(1,id,EFFECT_COUNT_CODE_OATH)
-	e1:SetTarget(s.target)
-	e1:SetOperation(s.activate)
+	e1:SetHintTiming(0,TIMINGS_CHECK_MONSTER_E)
+	e1:SetCountLimit(1,id)
+	e1:SetTarget(s.destg)
+	e1:SetOperation(s.desop)
 	c:RegisterEffect(e1)
 	--Substitute destruction for "B.E.F. ZELOS"
 local e2=Effect.CreateEffect(c)
@@ -23,25 +24,25 @@ local e2=Effect.CreateEffect(c)
 	c:RegisterEffect(e2)
 end
 s.listed_series={0x15}
-function s.ddfilter(c)
+function s.desfilter(c,e,tp)
 	return c:IsFaceup() and c:IsSetCard(0x15)
 end
-function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(tp) and s.ddfilter(chkc) end
-	if chk==0 then return Duel.IsPlayerCanDraw(tp,2)
-		and Duel.IsExistingTarget(s.ddfilter,tp,LOCATION_MZONE,0,1,nil) end
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
-	local g=Duel.SelectTarget(tp,s.ddfilter,tp,LOCATION_MZONE,0,1,1,nil)
-	Duel.SetTargetPlayer(tp)
-	Duel.SetTargetParam(2)
-	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,1,0,0)
-	Duel.SetOperationInfo(0,CATEGORY_DRAW,nil,0,tp,2)
+function s.desrescon(sg,e,tp,mg)
+    local own=sg:FilterCount(Card.IsControler,nil,tp)
+    return own==1,own>1
 end
-function s.activate(e,tp,eg,ep,ev,re,r,rp)
-	local g,p,d=Duel.GetChainInfo(0,CHAININFO_TARGET_CARDS,CHAININFO_TARGET_PLAYER,CHAININFO_TARGET_PARAM)
-	g=g:Filter(Card.IsRelateToEffect,nil,e)
-	if #g>0 and Duel.Destroy(g,REASON_EFFECT)>0 then
-		Duel.Draw(p,d,REASON_EFFECT)
+function s.destg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return false end
+	local g=Duel.GetMatchingGroup(s.desfilter,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,nil,e,tp)
+	if chk==0 then return aux.SelectUnselectGroup(g,e,tp,3,3,s.desrescon,0) end
+	local dg=aux.SelectUnselectGroup(g,e,tp,3,3,s.desrescon,1,tp,HINTMSG_DESTROY)
+	Duel.SetTargetCard(dg)
+	Duel.SetOperationInfo(0,CATEGORY_DESTROY,dg,#dg,0,0)
+end
+function s.desop(e,tp,eg,ep,ev,re,r,rp)
+	local tg=Duel.GetTargetCards(e)
+	if #tg>0 then
+		Duel.Destroy(tg,REASON_EFFECT)
 	end
 end
 --banish
