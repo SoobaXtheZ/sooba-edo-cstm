f38d17fbee3c7f736091d2d527c2975e2b92fa43
SoobaXtheZ <104928372+SoobaXtheZ@users.noreply.github.com>
2022-08-01 10:52:03 +0200

    Update c489007.lua

--- script/unofficial/c489007.lua
+++ script/unofficial/c489007.lua
@@ -23,9 +23,8 @@ local e2=Effect.CreateEffect(c)
 	e2:SetOperation(s.repop)
 	c:RegisterEffect(e2)
 end
-s.listed_series={0x15}
-function s.desfilter(c,e,tp)
-	return c:IsFaceup() and c:IsSetCard(0x15)
+unction s.desfilter(c,e,tp)
+	return c:IsCanBeEffectTarget(e) and (c:IsControler(1-tp) or (c:IsFaceup() and c:IsMonster() and c:IsRace(RACE_MACHINE)))
 end
 function s.desrescon(sg,e,tp,mg)
     local own=sg:FilterCount(Card.IsControler,nil,tp)
